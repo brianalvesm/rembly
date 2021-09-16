422553afd2818e312372376be758d7fd9c5d098d
Brian Alves Moreira <hey@brianmoreira.com>
2021-09-16 17:43:47 -0300

    Sobe arquivos removendo diretório public

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass')(require('sass'));
 
 gulp.task('sass', function () {
-  return gulp.src('./public/sass/**/*.sass')
+  return gulp.src('./sass/**/*.sass')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/css/'));
+    .pipe(gulp.dest('./css/'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch('./public/sass/**/*.sass', gulp.series('sass'));
+  gulp.watch('./sass/**/*.sass', gulp.series('sass'));
 });
 
 gulp.task('default', gulp.series('watch', 'sass'));
