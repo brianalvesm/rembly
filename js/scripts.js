af090029a6bdafe36dfc365eddf8e6c36cd6b786
Brian Alves Moreira <hey@brianmoreira.com>
2021-09-28 00:18:26 -0300

    Finaliza página inicial

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,12 @@ $(document).foundation();
 $('.close-icon').click( function() {
 	$('#header-menu').hide();
 });
+
+$(window).on("scroll", function() {
+  if($(window).scrollTop() > 50) {
+    $(".header").addClass("active");
+  } else {
+    //remove the background property so it comes transparent again (defined in your css)
+  	$(".header").removeClass("active");
+  }
+});
